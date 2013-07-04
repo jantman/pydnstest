baa3efa29990b05c1f94d21be90da515d1309438
Jason Antman <jason@jasonantman.com>
2013-07-04 07:46:00 -0400

    exposing DNS server port options for future use in testing

--- dnstest.py
+++ dnstest.py
@@ -17,31 +17,31 @@ import DNS
 import pprint
 from inspect import getmembers
 
-def resolve_name(query, to_server, default_domain):
+def resolve_name(query, to_server, default_domain, to_port=53):
     """
     Resolves a single name against the given server
     """
 
     # first try an A record
-    s = DNS.Request(name=query, server=to_server, qtype='A')
+    s = DNS.Request(name=query, server=to_server, qtype='A', port=to_port)
     a = s.req()
     if len(a.answers) > 0:
         return {'answer': a.answers[0]}
 
     # if that didnt work, try a CNAME
-    s = DNS.Request(name=query, server=to_server, qtype='CNAME')
+    s = DNS.Request(name=query, server=to_server, qtype='CNAME', port=to_port)
     a = s.req()
     if len(a.answers) > 0:
         return {'answer': a.answers[0]}
     return {'status': a.header['status']}
 
-def lookup_reverse(name, to_server):
+def lookup_reverse(name, to_server, to_port=53):
     "convenience routine for doing a reverse lookup of an address"
     a = name.split('.')
     a.reverse()
     b = '.'.join(a)+'.in-addr.arpa'
 
-    s = DNS.Request(name=b, server=to_server, qtype='PTR')
+    s = DNS.Request(name=b, server=to_server, qtype='PTR', port=to_port)
     a = s.req()
     if len(a.answers) > 0:
         return {'answer': a.answers[0]}
