f34a4af2ef62b84668536ec0a33242976b628cc1
Jason Antman <jason@jasonantman.com>
2013-07-03 21:43:21 -0400

    figured out the pytest.mark.parametrize issue - the current docs at http://pytest.org/latest/parametrize.html#pytest-mark-parametrize-parametrizing-test-functions include an example like '@pytest.mark.parametrize("input,expected", ...' but the first argument should be a tuple of arg names, not a comma-separated list.

--- tests/dnstest_parser_test.py
+++ tests/dnstest_parser_test.py
@@ -19,7 +19,7 @@ class TestLanguageParsing:
     '(set )?reverse( for)? (record|name|entry)? <hostname_or_fqdn> to <hostname_or_fqdn>'
     """
 
-    @pytest.mark.parametrize("param,foo", [
+    @pytest.mark.parametrize(("line", "parsed_dict"), [
         ("add fooHostOne value fooHostTwo", {'operation': 'add', 'hostname': 'fooHostOne', 'value': 'fooHostTwo'}),
         ("add foobar value 10.104.92.243", {'operation': 'add', 'hostname': 'foobar', 'value': '10.104.92.243'}),
         ("add entry foobar with value baz", {'operation': 'add', 'hostname': 'foobar', 'value': 'baz'}),
@@ -27,15 +27,27 @@ class TestLanguageParsing:
         ("add name foobar address 192.168.0.139", {'operation': 'add', 'hostname': 'foobar', 'value': '192.168.0.139'}),
         ("add foobar.example.com with target 172.16.132.10", {'operation': 'add', 'hostname': 'foobar.example.com', 'value': '172.16.132.10'}),
         ("add foobar.hosts.example.com value 172.16.132.10", {'operation': 'add', 'hostname': 'foobar.hosts.example.com', 'value': '172.16.132.10'}),
+        ("remove fooHostOne", {'operation': 'remove', 'hostname': 'fooHostOne'}),
+        ("remove record fooHostOne", {'operation': 'remove', 'hostname': 'fooHostOne'}),
+        ("remove name fooHostOne", {'operation': 'remove', 'hostname': 'fooHostOne'}),
+        ("remove entry fooHostOne", {'operation': 'remove', 'hostname': 'fooHostOne'}),
+        ("remove foo.example.com", {'operation': 'remove', 'hostname': 'foo.example.com'}),
+        ("remove record foo.example.com", {'operation': 'remove', 'hostname': 'foo.example.com'}),
+        ("remove name foo.example.com", {'operation': 'remove', 'hostname': 'foo.example.com'}),
+        ("remove entry foo.example.com", {'operation': 'remove', 'hostname': 'foo.example.com'}),
+        ("remove entry foo.bar.baz.example.com", {'operation': 'remove', 'hostname': 'foo.bar.baz.example.com'}),
+
     ])
-    def test_parse_add_success(self, param, foo):
-        assert dnstest_parser.parse_line(param[0]).asDict() == param[1]
+    def test_parse_should_succeed(self, line, parsed_dict):
+        assert dnstest_parser.parse_line(line).asDict() == parsed_dict
     
-    @pytest.mark.parametrize("param", [
+    @pytest.mark.parametrize("line", [
         "add extraword record foobar.example.com target blam",
         "add foobar value blam extraword",
+        "remove foo blam",
+        "remove record 10.23.45.67",
+        "remove 10.23.45.67",
     ])
-    def test_parse_add_should_fail(self, param):
+    def test_parse_should_raise_exception(self, line):
         with pytest.raises(ParseException):
-            dnstest_parser.parse_line(param).asDict()
-
+            dnstest_parser.parse_line(line)
