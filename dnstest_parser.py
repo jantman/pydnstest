6786305e983576bf2d405dd32e18d6889c4507fb
Jason Antman <jason@jasonantman.com>
2013-07-03 21:27:09 -0400

    working on testing but still having issues with the pytest.mark.parametrize decorator

--- dnstest_parser.py
+++ dnstest_parser.py
@@ -17,16 +17,18 @@ from pyparsing import Word, alphas, alphanums, Suppress, Optional, Or, Regex, Li
 word = Word(alphas)
 value = Word(alphanums).setResultsName("value")
 add_op = Literal("add").setResultsName("operation")
-val_op = Or([Suppress("value"), Suppress("address"), Suppress("target")])
+rec_op = Or([Literal("record"), Literal("entry"), Literal("name")])
+val_op = Optional(Literal("with")) + Or([Literal("value"), Literal("address"), Literal("target")])
 
-fqdn = Regex("([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*")
+fqdn = Regex("(([a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])(\.([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]{0,61}[a-zA-Z0-9]))*)")
 ipaddr = Regex("(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])")
 hostname = Regex("([a-zA-Z0-9][a-zA-Z0-9\-]{0,62}[a-zA-Z0-9])")
+hostname_or_fqdn = Or([hostname, fqdn])
+hostname_fqdn_or_ip = Or([hostname, fqdn, ipaddr])
 
-cmd_add = add_op + hostname.setResultsName("hostname") + val_op + value
+cmd_add = add_op + Optional(rec_op) + hostname_or_fqdn.setResultsName("hostname") + val_op + hostname_fqdn_or_ip.setResultsName('value')
 
 def parse_line(line):
     res = cmd_add.parseString(line, parseAll=True)
     return res
 
-# up to slide 27 http://www.slideshare.net/Siddhi/creating-domain-specific-languages-in-python
--- tests/dnstest_parser_test.py
+++ tests/dnstest_parser_test.py
@@ -7,17 +7,29 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../')
 import dnstest_parser
 from pyparsing import ParseException
 
-class TestParseAdd:
+class TestLanguageParsing:
+    """
+    Class to test the natural language parsing features of dnstest.py
 
-    @pytest.mark.parametrize("param", [
+    Grammars supported:
+    'add (record|name|entry)? <hostname_or_fqdn> (with ?)(value|address|target)? <hostname_fqdn_or_ip>'
+    'remove (record|name|entry)? <hostname_or_fqdn>'
+    'rename (record|name|entry)? <hostname_or_fqdn> to <hostname_or_fqdn>'
+    'change (record|name|entry)? <hostname_or_fqdn> to <hostname_fqdn_or_ip>'
+    '(set )?reverse( for)? (record|name|entry)? <hostname_or_fqdn> to <hostname_or_fqdn>'
+    """
+
+    @pytest.mark.parametrize("param,foo", [
         ("add fooHostOne value fooHostTwo", {'operation': 'add', 'hostname': 'fooHostOne', 'value': 'fooHostTwo'}),
         ("add foobar value 10.104.92.243", {'operation': 'add', 'hostname': 'foobar', 'value': '10.104.92.243'}),
-        ("add host foobar with value baz", {'operation': 'add', 'hostname': 'foobar', 'value': 'baz'}),
+        ("add entry foobar with value baz", {'operation': 'add', 'hostname': 'foobar', 'value': 'baz'}),
         ("add record foobar.example.com target blam", {'operation': 'add', 'hostname': 'foobar.example.com', 'value': 'blam'}),
+        ("add name foobar address 192.168.0.139", {'operation': 'add', 'hostname': 'foobar', 'value': '192.168.0.139'}),
+        ("add foobar.example.com with target 172.16.132.10", {'operation': 'add', 'hostname': 'foobar.example.com', 'value': '172.16.132.10'}),
+        ("add foobar.hosts.example.com value 172.16.132.10", {'operation': 'add', 'hostname': 'foobar.hosts.example.com', 'value': '172.16.132.10'}),
     ])
-    def test_parse_add_success(self, param):
+    def test_parse_add_success(self, param, foo):
         assert dnstest_parser.parse_line(param[0]).asDict() == param[1]
-    
     
     @pytest.mark.parametrize("param", [
         "add extraword record foobar.example.com target blam",
@@ -26,3 +38,4 @@ class TestParseAdd:
     def test_parse_add_should_fail(self, param):
         with pytest.raises(ParseException):
             dnstest_parser.parse_line(param).asDict()
+
