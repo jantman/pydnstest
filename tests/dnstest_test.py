34a24e3e354cb99a97acbdc8f7a59cf3e1e3e98e
Jason Antman <jason@jasonantman.com>
2013-07-04 19:43:35 -0400

    adding the beginning of a test class for the dnstest.py DNS checking functions, including stub methods to return static DNS data

---
+++ tests/dnstest_test.py
@@ -0,0 +1,42 @@
+# tests for dns_parser.py
+
+import pytest
+import sys, os
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../')
+
+import dnstest
+
+class TestDNSrules:
+    """
+    Test DNS tests, using stubbed name resolution methods that return static values.
+
+    The code in this class checks the logic of dnstest.py's test_*_name methods, which take
+    input describing the change, and query nameservers to check current prod and staging status.
+    """
+
+    def stub_resolve_name(query, to_server, default_domain, to_port = 53):
+        """
+        stub method
+
+        return a dict that looks like the return value from dnstest.resolve_name
+        but either returns one of a hard-coded group of dicts, or an error.
+        """
+
+        #return {'answer': a.answers[0]}
+        return {'status': a.header['status']}
+
+    # stub
+    dnstest.resolve_name = stub_resolve_name
+
+    def stub_lookup_reverse(name, to_server, to_port=53):
+        """
+        stub method
+
+        return a dict that looks like the return value from dnstest.lookup_reverse
+        but either returns one of a hard-coded group of dicts, or an error.
+        """
+        #return {'answer': a.answers[0]}
+        return {'status': a.header['status']}
+
+    # stub
+    dnstest.lookup_reverse = stub_lookup_reverse
