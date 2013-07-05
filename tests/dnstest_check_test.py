abc418f02e7b0a255737f083eaca5814656e06da
Jason Antman <jason@jasonantman.com>
2013-07-05 09:12:02 -0400

    rename class to be autodiscovered by py.test

--- tests/dnstest_check_test.py
+++ tests/dnstest_check_test.py
@@ -8,7 +8,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)) + '/../')
 import dnstest
 
 
-class DNScheck:
+class TestDNSChecks:
     """
     Test DNS checks, using stubbed name resolution methods that return static values.
 
