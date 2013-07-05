e3c5bc39fe2ec18172a9427a54fe833570db02f8
Jason Antman <jason@jasonantman.com>
2013-07-05 08:21:31 -0400

    refactor dns query methods into own module

--- dnstest.py
+++ dnstest.py
@@ -14,38 +14,10 @@ ToDo: flag to confirm against prod once live
 """
 
 import DNS
+from dnstest_dns import resolve_name, lookup_reverse
 
 
-def resolve_name(query, to_server, default_domain, to_port=53):
-    """
-    Resolves a single name against the given server
-    """
-
-    # first try an A record
-    s = DNS.Request(name=query, server=to_server, qtype='A', port=to_port)
-    a = s.req()
-    if len(a.answers) > 0:
-        return {'answer': a.answers[0]}
-
-    # if that didnt work, try a CNAME
-    s = DNS.Request(name=query, server=to_server, qtype='CNAME', port=to_port)
-    a = s.req()
-    if len(a.answers) > 0:
-        return {'answer': a.answers[0]}
-    return {'status': a.header['status']}
-
-
-def lookup_reverse(name, to_server, to_port=53):
-    "convenience routine for doing a reverse lookup of an address"
-    a = name.split('.')
-    a.reverse()
-    b = '.'.join(a) + '.in-addr.arpa'
-
-    s = DNS.Request(name=b, server=to_server, qtype='PTR', port=to_port)
-    a = s.req()
-    if len(a.answers) > 0:
-        return {'answer': a.answers[0]}
-    return {'status': a.header['status']}
+config = {}
 
 
 def check_removed_names(removed, test_server, prod_server, default_domain, have_reverse):
