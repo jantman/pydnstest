2c930b668a2195af0d499f71a2f39e951b7ff5f8
Jason Antman <jason@jasonantman.com>
2013-11-02 17:43:57 -0400

    first try at a setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from sys import version_info
+
+if version_info.major == 3:
+    pyver_requires=[
+        "py3dns==3.0.4",
+        "pyparsing==2.0.1",
+    ]
+else:
+    pyver_requires=[
+        "pydns==2.3.6",
+        "pyparsing==1.5.7",
+    ]
+
+setup(
+    name='pydnstest',
+    version='0.1.0',
+    author='Jason Antman',
+    author_email='jason@jasonantman.com',
+    packages=['pydnstest', 'pydnstest.test'],
+    scripts=['bin/dnstest'],
+    url='http://github.com/jantman/pydnstest/',
+    license='LICENSE.txt',
+    description='Tool to test DNS changes on a staging server and verify in production',
+    long_description='Python tool for testing DNS changes (add, remove, rename, change records) against a staging DNS server, verifying the same changes against production, or confirming that a record returns the same result in both environments.',
+    install_requires=pyver_requires
+)
