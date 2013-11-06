8311cc32f8f4f6b03bd7b2f51d5293076e8171b9
Jason Antman <Jason.Antman@coxinc.com>
2013-11-05 20:50:28 -0500

    fix the rest of the sys.version_info.major to be py26-compatible

--- pydnstest/tests/dnstest_config_test.py
+++ pydnstest/tests/dnstest_config_test.py
@@ -45,7 +45,7 @@ import os.path
 import os
 
 # conditional imports for packages with different names in python 2 and 3
-if sys.version_info.major == 3:
+if sys.version_info[0] == 3:
     import configparser as ConfigParser
     from configparser import ParsingError
 else:
